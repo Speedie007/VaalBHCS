4d880b0eda346895d92247e145fc53a9a01832a8
Brendan Wood <Brendanw@mweb.co.za>
2019-02-06 21:31:53 +0200

    Latest Version

--- VaalBHCS.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace VaalBHCS.Models
-{
-    public class Class1
-    {
-    }
-}
---
+++ VaalBHCSSystem/Areas/Admin/Controllers/Home/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VaalBHCSSystem.Areas.Admin.Controllers.Home
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
